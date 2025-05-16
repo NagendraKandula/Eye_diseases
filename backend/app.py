e25ca77e927e1bddd574250e2fa6ea8d734bb32c
NagendraKandula <139203478+NagendraKandula@users.noreply.github.com>
2025-05-16 19:53:16 +0530

    app.py

--- backend/app.py
+++ backend/app.py
@@ -56,4 +56,5 @@ def suggest_remedy(class_name):
     return remedies.get(class_name, 'No specific remedy found.')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+
